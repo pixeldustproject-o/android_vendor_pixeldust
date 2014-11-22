c171d04d12b258515bc0921882a61aa33b14f0e5
myfluxi <linflux@arcor.de>
2014-11-22 19:33:26 +0100

    vendor: Update SELinux policy for sysinit -updated for 8.1
    Signed-off-by: spezi77 <spezi77@gmx.com>

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,10 @@
+#
+# This policy configuration will be used by all products that
+# inherit from pixeldust
+#
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/pixeldust/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/pixeldust/sepolicy/private
