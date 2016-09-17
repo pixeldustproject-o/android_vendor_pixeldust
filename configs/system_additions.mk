def67a01f8b6094311913b838d56f6897b9e43c0
spezi77 <spezi77@gmx.com>
2016-09-17 11:16:40 +0200

    Allow insecure ADB logging at boot.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -29,3 +29,10 @@ PRODUCT_COPY_FILES += \
 # Security Enhanced Linux
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
+
+# Allow recording ADB logcat during boot phase
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    persist.service.adb.enable=1
+
