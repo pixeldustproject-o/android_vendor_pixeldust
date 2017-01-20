voidz777-pure/android_vendor_pure

sepolicy: Start Pure Common sepolicy
Rather than having to maintain out of tree changes, it is often
easier to maintain a hiearchy of changes, starting with the vendors
common config file. From there, inheriting products can pick up a base
and start to add or remove certain bits from it, making use of the
BOARD_SEPOLICY_* functions documented in external/sepolicy/README.

sepolicy/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Pure
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/pure/sepolicy

pixeldustproject-o/android_vendor_pixeldust

Signed-off-by: spezi77 <spezi77@gmx.com>

sepolicy/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from pixeldust
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/pixeldust/sepolicy