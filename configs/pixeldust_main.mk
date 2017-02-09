71c41cbbde0e951347363fe8714c71954f9aacea
spezi77 <spezi77@gmx.com>
2017-02-09 20:56:09 +0100

    Jack server heap size increase.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/pixeldust_main.mk
+++ configs/pixeldust_main.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Jack server heap size
+export ANDROID_JACK_VM_ARGS += "-Xmx4096m"
+
 # Include overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/pixeldust/overlay/common
