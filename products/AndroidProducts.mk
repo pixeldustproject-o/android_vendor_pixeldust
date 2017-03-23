48da36abad606918c74e28614bdb363acd928f4b
spezi77 <spezi77@gmx.com>
2017-03-23 13:14:22 +0100

    Products: add Pixel devices.
    Signed-off-by: spezi77 <spezi77@gmx.com>

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,21 @@
+# Copyright (C) 2017 The Pixel Dust Project
+#
+# Licensed under the Apurityche License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apurityche.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (pixeldust_marlin,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pixeldust_marlin.mk
+endif
+
+ifeq (pixeldust_sailfish,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pixeldust_sailfish.mk
+endif
