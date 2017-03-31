fb0d40c86cdd2ae749889f6444b2b74f5b22e9a8
spezi77 <spezi77@gmx.com>
2017-03-31 15:33:43 +0200

    Products: add support for bullhead (LGE Nexus 5X).
    @Funny-Bunny here you go!
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

---
+++ products/pixeldust_bullhead.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2017 The PixelDust Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit AOSP device configuration for bullhead.
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Include common PixelDust stuff
+include vendor/pixeldust/configs/pixeldust_phone.mk
+
+# Setup device specific product configuration.
+PRODUCT_NAME := pixeldust_bullhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := bullhead
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.1/N4F26T/3687331:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 N4F26T 3687331 release-keys"
+
