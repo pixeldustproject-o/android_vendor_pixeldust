f28da6d8dce2a62548aef103bb467dd8c528cbb4
Matthew Alex <matthewalex@outlook.com>
2017-04-06 02:32:35 -0400

    sailfish: update fingerprint to 7.1.2
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- products/pixeldust_sailfish.mk
+++ products/pixeldust_sailfish.mk
@@ -27,6 +27,6 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.1/NOF27C/3687810:user/release-keys \
-    PRIVATE_BUILD_DESC="sailfish-user 7.1.1 NOF27C 3687810 release-keys"
+    BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.2/N2G47E/3766409:user/release-keys \
+    PRIVATE_BUILD_DESC="sailfish-user 7.1.2 N2G47E 3766409 release-keys"
 
