5d378304e1aba64c46a07cc08a44ea69fa0f13f9
Funny-Bunny <dragos281993@gmail.com>
2017-04-07 23:27:12 +0300

    bullhead: change bootanimation
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -13,7 +13,12 @@
 # limitations under the License.
 
 # Add Pixel Dust ROM bootanimation based on device
-ifneq ($(filter pixeldust_angler pixeldust_bullhead pixeldust_marlin pixeldust_sailfish,$(TARGET_PRODUCT)),)
+ifneq ($(filter pixeldust_angler pixeldust_marlin pixeldust_sailfish,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/pixeldust/prebuilt/bootanimation/1440.zip:system/media/bootanimation.zip
 endif
+ifneq ($(filter pixeldust_bullhead,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/pixeldust/prebuilt/bootanimation/1080.zip:system/media/bootanimation.zip
+endif
+
