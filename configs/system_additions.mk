voidz777-pure/android_vendor_pure

Modular backuptool.sh
Tips & Tricks
=============
* 50-base.sh contains only a reference implementation.  You may customize the methods however you wish.
  For example, 20-foobar.sh pre-backup can use a loop with conditionals to generate a dynamic backup list in
  /tmp/foobar_file_list which is later printed by list_files() so the backup method will act on those files.
* Optional methods pre-backup, post-backup, pre-restore, or post-restore may be defined for special purposes.
* Inject new files into /tmp/addon.d/ prior to backuptool.sh backup if you want to act during the current system upgrade.
* Delete files from /tmp/addon.d/ during post-restore if you want to permanently remove files from /system/addon.d/
  Addons may use this approach to run a script only once.
* Scripts run in sort -n order.  Prefix with numbers 00 through 99 if want to run in a particular order.
* You can have two separate scripts, implementing only backup in one, and only restore in the other with a different
  number prefix of each.  This allows even greater control the backup/restore order even further.
* You could use pre-backup to generate a one-time use backup script in /tmp/addon.d/ that deletes itself in
  post-restore.

configs/system_additions.mk
@@ -12,12 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include vendor/nexus/configs/aosp_fixes.mk
-include vendor/nexus/configs/nexus_main.mk
-include vendor/nexus/configs/system_additions.mk
-
-# Telephony packages
-PRODUCT_PACKAGES += \
-    Stk \
-    CellBroadcastReceiver
-
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/nexus/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/nexus/prebuilt/bin/50-base.sh:system/addon.d/50-base.sh \

pixeldustproject-o/android_vendor_pixeldust

Signed-off-by: spezi77 <spezi77@gmx.com>

configs/system_additions.mk
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/pixeldust/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/pixeldust/prebuilt/bin/50-base.sh:system/addon.d/50-base.sh
+
 # init.d script support
 PRODUCT_COPY_FILES += \
     vendor/pixeldust/prebuilt/bin/sysinit:system/bin/sysinit