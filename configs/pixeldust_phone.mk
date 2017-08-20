99b217832af3d3770e0e0bb2f9b94198ebd0ce78
spezi77 <spezi77@gmx.com>
2017-08-20 13:07:33 +0200

    Add latest World APN & SPN list from DU.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/pixeldust_phone.mk
+++ configs/pixeldust_phone.mk
@@ -18,6 +18,18 @@ include vendor/pixeldust/configs/pixeldust_main.mk
 include vendor/pixeldust/configs/system_additions.mk
 include vendor/pixeldust/configs/version.mk
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     Stk \
