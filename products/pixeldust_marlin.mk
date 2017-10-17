7d99c53e954ac356e226838e395c70ccb739daf9
spezi77 <spezi77@gmx.com>
2017-10-17 20:45:23 +0200

    Marlin: Update Fingerprint to OPR3.170623.008
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- products/pixeldust_marlin.mk
+++ products/pixeldust_marlin.mk
@@ -30,6 +30,6 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:8.0.0/OPR6.170623.012/4283428:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 8.0.0 OPR6.170623.012 4283428 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:8.0.0/OPR3.170623.008/4294783:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 8.0.0 OPR3.170623.008 4294783 release-keys"
 
