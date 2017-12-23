9a8042804e6fb400fddb7022fbfae3e1c6e1c563
ElDainosor <eldainosor@gmail.com>
2017-12-23 03:55:35 +0000

    [2/2] Add 'GoBold' font family
    Required for lockscreen. Also, added in case of adding more fonts.
    
    based on this commit, credits to beanstown and jhenrique
    https://gitlab.com/PixelExperience/vendor_pixelstyle/commit/faa3d8365bc1fa0d885e3e98f1e609f6e0fb85fb
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -135,3 +135,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     GoogleMarkup
 
+# Additional System fonts
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/pixeldust/prebuilt/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/pixeldust/prebuilt/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/pixeldust/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/pixeldust/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/pixeldust/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
+
