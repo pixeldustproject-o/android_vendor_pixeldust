c2af93b7bbdbe54c10dfccfd9714d3ca2ed9b7d0
spezi77 <spezi77@gmx.com>
2018-03-01 20:47:49 +0100

    Additional ART options.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/optimizations.mk
+++ configs/optimizations.mk
@@ -29,10 +29,29 @@ PRODUCT_PROPERTY_OVERRIDES += \
     windowsmgr.max_events_per_sec=150 \
     ro.storage_manager.enabled=true
 
-# ART changes
+# Don't build debug for host or device
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+
+# Default compiler
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed
+
+# Preopt apks like GMS chimara modules
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Create profile for boot image
+# Outputs to /system/etc/boot-image.prof
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+
+# Speed apps
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+  SystemUI \
+  Settings \
+  NexusLauncherPrebuilt \
+  GoogleCamera
 
 # DragonTC info
 DRAGONTC_VERSION := 7.0
