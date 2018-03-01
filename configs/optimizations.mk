37a794b1f651c2db4946d100fbf97eb81f4a1986
spezi77 <spezi77@gmx.com>
2018-03-01 20:45:22 +0100

    Move and cleanup some old stuff which isn't used.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/optimizations.mk
+++ configs/optimizations.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 The Pixel Dust Project
+# Copyright (C) 2018 The Pixel Dust Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,22 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Toolchain and other
-TARGET_GCC_VERSION_KERNEL := 8.0
-
 # Prop Optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.performance.tuning=1 \
-    ro.facelock.black_timeout=400 \
-    ro.facelock.det_timeout=1500 \
-    ro.facelock.rec_timeout=2500 \
-    ro.facelock.lively_timeout=2500 \
-    ro.facelock.est_max_time=600 \
-    ro.facelock.use_intro_anim=false \
-    ro.max.fling_velocity=12000 \
-    ro.min.fling_velocity=8000 \
-    windowsmgr.max_events_per_sec=150 \
-    ro.storage_manager.enabled=true
+    keyguard.no_require_sim=true
 
 # Don't build debug for host or device
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
