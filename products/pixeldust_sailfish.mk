7006807815cc3ee9b4f2d68b6b93d6ffb25352fe
spezi77 <spezi77@gmx.com>
2018-03-11 23:42:36 +0100

    products: Update marlin/sailfish build desc for OPM1.171019.021
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- products/pixeldust_sailfish.mk
+++ products/pixeldust_sailfish.mk
@@ -39,8 +39,8 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    BUILD_FINGERPRINT=google/sailfish/sailfish:8.1.0/OPM1.171019.016/4503492:user/release-keys \
-    PRIVATE_BUILD_DESC="sailfish-user 8.1.0 OPM1.171019.016 4503492 release-keys"
+    BUILD_FINGERPRINT=google/sailfish/sailfish:8.1.0/OPM1.171019.021/4565141:user/release-keys \
+    PRIVATE_BUILD_DESC="sailfish-user 8.1.0 OPM1.171019.021 4565141 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pixeldust.maintainer="spezi77"
