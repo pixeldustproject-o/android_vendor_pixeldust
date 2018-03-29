56708fc35cf67eb70442db3e31255c4039b6a895
Mrinal Ghosh <31700322+mg712702@users.noreply.github.com>
2018-03-30 03:51:17 +0530

    No longer include Pixel proprietary apps.
    With new caf manifest, we fixed the no launcher & some other apps like dialer issue.
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- products/pixeldust_Z00L.mk
+++ products/pixeldust_Z00L.mk
@@ -21,9 +21,6 @@ include vendor/pixeldust/configs/pixeldust_phone.mk
 # Include optional stuff (e.g. prebuilt apps)
 include vendor/pixeldust/configs/system_optional.mk
 
-# Include Pixel proprietary apps
-include vendor/proprietary_pixel/pixelapps.mk
-
 # Setup device specific product configuration.
 PRODUCT_NAME := pixeldust_Z00L
 PRODUCT_DEVICE := Z00L
