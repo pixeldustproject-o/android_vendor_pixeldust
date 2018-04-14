17ddc0a080fb8d361dc17c8a45bebf9cc67e0217
spezi77 <spezi77@gmx.com>
2018-04-14 09:20:33 +0200

    SELinux policy: Use the one directory AOSP allows for SEPOLICY_DIRS. - merged our policy files. - fix compiler errors:
        FAILED: The following types on /data/ must be associated with the
        "data_file_type" attribute: userinit_data_exec
    
        FAILED: libsepol.context_from_record: type recovery_prop is not
        defined
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -3,8 +3,5 @@
 # inherit from pixeldust
 #
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    vendor/pixeldust/sepolicy/public
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    vendor/pixeldust/sepolicy/private
+BOARD_SEPOLICY_DIRS += \
+    vendor/pixeldust/sepolicy
