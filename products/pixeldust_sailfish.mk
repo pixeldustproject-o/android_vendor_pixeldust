718c597921228613455ef5280aba66ac06498220
spezi77 <spezi7713@gmx.net>
2018-05-13 15:21:10 +0200

    products: sailfish: Do not use DragonTC.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/pixeldust_sailfish.mk
+++ products/pixeldust_sailfish.mk
@@ -15,6 +15,10 @@
 # Inherit AOSP device configuration for marlin.
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 
+# Do not use DragonTC for this device
+DISABLE_DTC_OPTS := true
+TARGET_USE_SDCLANG := true
+
 # Include common PixelDust stuff
 include vendor/pixeldust/configs/pixeldust_phone.mk
 
