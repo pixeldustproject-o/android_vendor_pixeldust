e3344283a84710e2050bfbe9fc42ce5b2631b094
spezi77 <spezi7713@gmx.net>
2018-05-13 15:18:32 +0200

    pixeldust_main: Export a flag to use the default language for output. Idk why this compiler error suddenly popped up, but this is going to solve the compiler error below.
    FAILED:
    /home/spezi77/android/pdo/out/target/product/marlin/obj/STATIC_LIBRARIES/libedify_intermediates/lexer.cpp
    /bin/bash -c "prebuilts/misc/linux-x86/flex/flex-2.5.39
    -o/home/spezi77/android/pdo/out/target/product/marlin/obj/STATIC_LIBRARIES/libedify_intermediates/lexer.cpp
    bootable/recovery/edify/lexer.ll"
    flex-2.5.39: loadlocale.c:130: _nl_intern_locale_data: Assertion `cnt <
    (sizeof (_nl_value_type_LC_TIME) / sizeof (_nl_value_type_LC_TIME[0]))'
    failed.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/pixeldust_main.mk
+++ configs/pixeldust_main.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Use the default language for output
+export LC_ALL=C
+
 # Include overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/pixeldust/overlay/common
