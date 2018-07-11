d48ee060b1abdda1b38e031f3d27ec2ae04c57c0
spezi77 <spezi7713@gmx.net>
2018-07-11 12:17:47 +0200

    Dex pre-opt: Remove all the crap.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/optimizations.mk
+++ configs/optimizations.mk
@@ -19,10 +19,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Don't build debug for host or device
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-ART_BUILD_TARGET_NDEBUG := true
-ART_BUILD_TARGET_DEBUG := false
-ART_BUILD_HOST_NDEBUG := true
-ART_BUILD_HOST_DEBUG := false
 
 # Dex pre-opt
 WITH_DEXPREOPT := true
@@ -33,17 +29,6 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed
 
 # Preopt apks like GMS chimara modules
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-
-# Create profile for boot image
-# Outputs to /system/etc/boot-image.prof
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-
-# Speed apps
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-  SystemUI \
-  Settings \
-  NexusLauncherPrebuilt \
-  GoogleCamera
 
 # Conditionally use either DragonTC OR SDCLANG Optimizations (in order to disable the use of DragonTC the relevant flags used below need to be inserted into your device make file in vendor/pixeldust/products)
 ifneq ($(DISABLE_DTC_OPTS),true)
