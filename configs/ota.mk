3d9fae639fd63f756b1d93f97fba6de306225ec1
Nitin1438 <nitin1438@users.noreply.github.com>
2018-08-05 17:10:55 +0530

    Add taimen in OTA list

--- configs/ota.mk
+++ configs/ota.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter pixeldust_marlin,$(TARGET_PRODUCT)),)
+ifneq ($(filter pixeldust_marlin pixeldust_taimen,$(TARGET_PRODUCT)),)
 
 PRODUCT_PACKAGES += \
     PixeldustOTA
